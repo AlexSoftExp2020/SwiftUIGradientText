011094b2f980c9848689bdd9511d6d9c5a80f640
Alex <oleksii.pidhornyi@gmail.com>
2024-10-22 11:49:37 +0200

    Initial Commit

---
+++ SwiftUIGradientText/SwiftUIGradientTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGradientTextApp.swift
+//  SwiftUIGradientText
+//
+//  Created by Oleksii on 22.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGradientTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
